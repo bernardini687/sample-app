0d2480b2d3d327f1277677d6aec037cf86b6eb84
Oscar Bernardini <bernardini687@gmail.com>
2019-04-02 16:31:16 +0200

    Change root path and test it, finish Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,11 +5,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = ' | SampleApp'
   end
 
-  test 'should get home' do
-    get static_pages_home_url
+  test 'should get root' do
+    get root_url
     assert_response :success
     assert_select 'title', "Home#{@base_title}"
   end
+
+  # test 'should get home' do
+  #   get static_pages_home_url
+  #   assert_response :success
+  #   assert_select 'title', "Home#{@base_title}"
+  # end
 
   test 'should get help' do
     get static_pages_help_url
