030205abab9cd2c9a062d508e72766b0e8450c0b
Oscar Bernardini <bernardini687@gmail.com>
2019-04-02 16:12:15 +0200

    Refactor Static Pages views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @base_title = ' | SampleApp'
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', 'Home | SampleApp'
+    assert_select 'title', "Home#{@base_title}"
   end
 
   test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', 'Help | SampleApp'
+    assert_select 'title', "Help#{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'About | SampleApp'
+    assert_select 'title', "About#{@base_title}"
   end
 end
