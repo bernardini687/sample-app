d2fb8883aa663214f37dc62dfe69787753d786bf
Oscar Bernardini <bernardini687@gmail.com>
2019-04-03 11:03:51 +0200

    Define title helper and change tests accordingly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title = '', base_title = 'SampleApp')
+    page_title = '' if %w[home].include?(page_title)
+    page_title.empty? ? base_title : "#{page_title.capitalize} | #{base_title}"
+  end
 end
