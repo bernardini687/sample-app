e7a689936c01e4680d3c0e28bca73b94eccce5af
Oscar Bernardini <bernardini687@gmail.com>
2019-04-04 13:24:10 +0200

    Add custom prefixes to routes and modify test accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  # get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'static_pages/help', as: :help
+  get 'static_pages/about', as: :about
+  get 'static_pages/contact', as: :contact
 end
