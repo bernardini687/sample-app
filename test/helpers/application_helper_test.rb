8e5d52908b24f052546f42692a3946abff22c105
Oscar Bernardini <bernardini687@gmail.com>
2019-04-04 14:16:32 +0200

    Add Application Helper test and fix Static Pages controller test accordingly

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title helper' do
+    assert_equal full_title,                    'SampleApp'
+    assert_equal full_title('home'),            'SampleApp'
+    assert_equal full_title('help'),            'Help | SampleApp'
+    assert_equal full_title('test', 'Example'), 'Test | Example'
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
@@ -9,4 +9,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
