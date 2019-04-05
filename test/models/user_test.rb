1141f1071da30bda8d940adef341b072b8473311
Oscar Bernardini <bernardini687@gmail.com>
2019-04-05 11:05:31 +0200

    Add validations for name and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,52 @@ class UserTest < ActiveSupport::TestCase
   test 'should be valid' do
     assert @user.valid?
   end
+
+  test 'name should be present' do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+
+  test 'name should be less than 33 characters' do
+    @user.name = 'a' * 33
+    assert_not @user.valid?
+  end
+
+  test 'email should be less than 256 characters' do
+    @user.email = 'a' * 247 + '@user.com'
+    assert_not @user.valid?
+  end
+
+  test 'email validation should accept valid addresses' do
+    %w[
+      user@example.com
+      USER@foo.COM
+      A_US-ER@foo.bar.org
+      first.last@foo.jp
+      alice+bob@baz.cn
+    ].each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    %w[
+      user@example,com
+      user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com
+      foo@bar+baz.com
+      foo@bar..com
+      foo@bar.coding
+    ].each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
 end
