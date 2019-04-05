c0ac883b80c7ee2ee3d0d82c6712077c548b3811
Oscar Bernardini <bernardini687@gmail.com>
2019-04-05 14:20:58 +0200

    Add index to users email and before_save callback for name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{1,4}\z/i.freeze
+
+  before_save do
+    name.capitalize!
+    email.downcase!
+  end
+
   validates :name,  presence: true,
                     length: { maximum: 32 }
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{1,4}\z/i.freeze
 
   validates :email, presence: true,
                     length: { maximum: 255 },
