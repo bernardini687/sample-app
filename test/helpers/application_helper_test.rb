0bd8b8c0d41288fdf30b8e5fbc9ad45a3255049c
Oscar Bernardini <bernardini687@gmail.com>
2019-04-07 10:24:41 +0200

    Fix full_title helper

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test 'full title helper' do
-    assert_equal full_title,                    'SampleApp'
-    assert_equal full_title('help'),            'Help | SampleApp'
-    assert_equal full_title('test', 'Example'), 'Test | Example'
+    assert_equal full_title,         'SampleApp'
+    assert_equal full_title('help'), 'Help | SampleApp'
   end
 end
