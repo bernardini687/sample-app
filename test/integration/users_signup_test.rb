15c943616ea442b20239a1c195fd495b08404299
Oscar Bernardini <bernardini687@gmail.com>
2019-04-07 14:34:43 +0200

    Improve custom helpers, tests and flashes partial

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,8 +10,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
-    assert_select '#error_explanation'
-    assert_select '.field_with_errors'
-    assert_select 'form[action="/signup"]'
+    assert_select   '#error_explanation'
+    assert_select   '.field_with_errors'
+    assert_select   'form[action="/signup"]'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: 'rails tutorial',
+                                          email: 'example@railstutorial.org',
+                                          password: 'asdfjk',
+                                          password_confirmation: 'asdfjk' } }
+    end
+    follow_redirect! # get user_path(User.first)
+    assert_template 'users/show'
+    assert_select   '.alert-success'
+    assert_select   'title', full_title(User.first.name)
   end
 end
