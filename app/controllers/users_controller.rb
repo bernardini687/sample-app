13e4ded1726bf1a023bbdb8f445a30ecaee015a0
Oscar Bernardini <bernardini687@gmail.com>
2019-04-07 13:59:20 +0200

    Valid users can Sign up; add partial for flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # redirect
+      flash[:success] = 'Welcome to SampleApp!'
+      redirect_to @user
     else
       render 'new'
     end
@@ -19,6 +20,9 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
   end
 end
