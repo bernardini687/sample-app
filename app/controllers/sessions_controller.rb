e73b8248635bc14465d7cd1fe2b669237fbd0ecf
Oscar Bernardini <bernardini687@gmail.com>
2019-04-08 11:34:27 +0200

    Add failed Log in notices and Log in integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,18 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # redirect_to user
+      # flash[:success] = 'YAAAAAY!'
+    else
+      if user
+        flash[:danger] = 'NOOOOO! Bad password!'
+      else
+        flash[:danger] = 'NOOOOO! Bad email!'
+      end
+      render 'new'
+    end
   end
 
   def destroy; end
