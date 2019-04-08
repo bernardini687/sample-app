4402c1c810c915829f05fb9f6407b472356e1f06
Oscar Bernardini <bernardini687@gmail.com>
2019-04-08 13:54:43 +0200

    Add fixture for testing a successful Log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,18 @@ class User < ApplicationRecord
   end
 
   validates :name,  presence: true, length: { maximum: 32 }
-
   validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
