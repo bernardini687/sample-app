4e9aa5c81038cabc456af3882be51943ca069809
Oscar Bernardini <bernardini687@gmail.com>
2019-04-10 10:59:01 +0200

    Tests for checkbox logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,16 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+    # Set @user so users_login_test can retrieve it with the assigns method
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       if params[:session].values.uniq == ['']
         flash.now[:danger] = 'NOOOOO! Type something!'
-      elsif user
+      elsif @user
         flash.now[:danger] = 'NOOOOO! Bad password!'
       else
         flash.now[:danger] = 'NOOOOO! Bad email!'
