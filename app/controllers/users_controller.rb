ecda034fee447a377d194fa4eef79d9ebab96d51
Oscar Bernardini <bernardini687@gmail.com>
2019-04-10 16:53:17 +0200

    Prevent users to update each other's informations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user,   only: %i[edit update]
 
   def show
     @user = User.find(params[:id])
@@ -20,12 +21,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -49,5 +47,11 @@ class UsersController < ApplicationController
       flash[:danger] = 'Please, log in!'
       redirect_to login_url
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:oscar)
+    @other = users(:archer)
   end
 
   test 'should get signup' do
@@ -22,5 +23,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@other)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@other)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
