a16fa9c70ac893b5b66e0673146b7554cc65d398
Oscar Bernardini <bernardini687@gmail.com>
2019-04-10 10:31:03 +0200

    Forget users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
-      if params[:session].values.uniq == [""]
+      if params[:session].values.uniq == ['']
         flash.now[:danger] = 'NOOOOO! Type something!'
       elsif user
         flash.now[:danger] = 'NOOOOO! Bad password!'
@@ -19,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,12 @@ class User < ApplicationRecord
     email.downcase!
   end
 
+  validates :name, presence: true, length: { maximum: 32 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{1,4}\z/i.freeze
-  validates :name,  presence: true, length: { maximum: 32 }
   validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
@@ -37,5 +36,17 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
