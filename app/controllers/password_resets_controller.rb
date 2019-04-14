4f7dd77ea6d21bcd5a6f27035de5adbd6bb776b8
Oscar Bernardini <bernardini687@gmail.com>
2019-04-14 08:49:34 +0200

    Work on password_resets#create action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,16 @@
 class PasswordResetsController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def edit
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = 'Email sent with password reset instructions.'
+      redirect_to root_url
+    else
+      flash[:danger] = "There's nobody with that email here."
+      render 'new'
+    end
   end
 end
