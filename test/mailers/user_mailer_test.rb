e459b2660ae545e811237f29e3e19a3e4c3b0f8b
Oscar Bernardini <bernardini687@gmail.com>
2019-04-14 09:08:50 +0200

    Fill in password reset emails and test for correct content

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,6 +4,7 @@ class UserMailerTest < ActionMailer::TestCase
   def setup
     @user = users(:oscar)
     @user.activation_token = User.new_token
+    @user.reset_token = User.new_token
   end
 
   test 'account_activation' do
@@ -13,6 +14,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ['noreply@example.com'], mail.from
     assert_match @user.name, mail.body.encoded
     assert_match @user.activation_token, mail.body.encoded
+    assert_match CGI.escape(@user.email), mail.body.encoded
+  end
+
+  test 'password_reset' do
+    mail = UserMailer.password_reset(@user)
+    assert_equal 'Password reset', mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ['noreply@example.com'], mail.from
+    assert_match @user.reset_token, mail.body.encoded
     assert_match CGI.escape(@user.email), mail.body.encoded
   end
 end
