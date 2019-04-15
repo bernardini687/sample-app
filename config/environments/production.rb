f0b6ad8bfaec566c2617a402688278af03f5741e
Oscar Bernardini <bernardini687@gmail.com>
2019-04-15 11:06:00 +0200

    Configure production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'sample233.herokuapp.com' }
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample-app_#{Rails.env}"
